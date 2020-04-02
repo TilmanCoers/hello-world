17ee27801482d7668b24bac68382003055412876
Tilman Coers <tilmancoers@gmail.com>
2020-04-02 16:07:18 +0200

    changed first.py

--- first.py
+++ first.py
@@ -1,2 +1,5 @@
-namee = input('Please type in your Name to say "Hello" to the world:')
-print('hello world by', namee)
+namee = input('Please type in your name to say "Hello" to the world:')
+if not isinstance(namee, str) is True:
+    print ('You did not enter your name')
+else:
+    print('hello world by', namee)
